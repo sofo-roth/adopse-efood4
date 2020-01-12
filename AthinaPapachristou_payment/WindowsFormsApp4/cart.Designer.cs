c1b0ef269b32a7f024adfc2effb245ed00cd741f
athina-pap <57074771+athina-pap@users.noreply.github.com>
2020-01-12 23:03:01 +0200

    Update cart.Designer.cs

--- AthinaPapachristou_payment/WindowsFormsApp4/cart.Designer.cs
+++ AthinaPapachristou_payment/WindowsFormsApp4/cart.Designer.cs
@@ -23,6 +23,7 @@
        
        
 
+       
         #region Windows Form Designer generated code
 
         /// <summary>
@@ -32,21 +33,13 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(cart));
-            this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
-            this.show = new System.Windows.Forms.Label();
-            this.item = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
+            this.s = new System.Windows.Forms.Button();
+            this.timi = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // panel1
-            // 
-            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
-            this.panel1.Location = new System.Drawing.Point(1, 1);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(156, 457);
-            this.panel1.TabIndex = 0;
             // 
             // panel2
             // 
@@ -60,31 +53,16 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
-            this.label1.Location = new System.Drawing.Point(220, 325);
+            this.label1.Location = new System.Drawing.Point(236, 317);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(99, 20);
+            this.label1.Size = new System.Drawing.Size(105, 20);
             this.label1.TabIndex = 2;
-            this.label1.Text = "Total price";
-            // 
-            // show
-            // 
-            this.show.AutoSize = true;
-            this.show.Location = new System.Drawing.Point(264, 205);
-            this.show.Name = "show";
-            this.show.Size = new System.Drawing.Size(0, 17);
-            this.show.TabIndex = 3;
-            // 
-            // item
-            // 
-            this.item.AutoSize = true;
-            this.item.Location = new System.Drawing.Point(237, 69);
-            this.item.Name = "item";
-            this.item.Size = new System.Drawing.Size(0, 17);
-            this.item.TabIndex = 4;
+            this.label1.Text = "Total price:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(452, 351);
+            this.button2.Location = new System.Drawing.Point(474, 396);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(106, 31);
             this.button2.TabIndex = 6;
@@ -92,18 +70,46 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // s
+            // 
+            this.s.Location = new System.Drawing.Point(12, 33);
+            this.s.Name = "s";
+            this.s.Size = new System.Drawing.Size(75, 23);
+            this.s.TabIndex = 8;
+            this.s.Text = "show cart";
+            this.s.UseVisualStyleBackColor = true;
+            this.s.Click += new System.EventHandler(this.s_Click);
+            // 
+            // timi
+            // 
+            this.timi.AutoSize = true;
+            this.timi.Location = new System.Drawing.Point(363, 320);
+            this.timi.Name = "timi";
+            this.timi.Size = new System.Drawing.Size(0, 17);
+            this.timi.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(474, 216);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "erase last";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // cart
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Bisque;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.timi);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.item);
-            this.Controls.Add(this.show);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.s);
             this.Controls.Add(this.panel2);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
             this.Name = "cart";
             this.Text = "cart";
             this.ResumeLayout(false);
@@ -112,12 +118,11 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label show;
-        private System.Windows.Forms.Label item;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button s;
+        private System.Windows.Forms.Label timi;
+        private System.Windows.Forms.Button button1;
     }
 }
